0d085230a433d564095b09244fcccb1e63ff064f
Malte Münchert <malte.muenchert@gmx.net>
2015-12-11 23:58:08 +0100

    Remve unecessary namespace addresses

--- lib/pocketops/cli.rb
+++ lib/pocketops/cli.rb
@@ -2,17 +2,17 @@ module Pocketops
   class CLI < Thor
     desc 'deploy', 'deploy the Rails app to the remote server'
     def deploy
-      Pocketops::Commands::Deploy.run
+      Commands::Deploy.run
     end
 
     desc 'init', 'create a deploy user and remove SSH access to root on remote server (run this once before you do anything else)'
     def init
-      Pocketops::Commands::Deploy.init
+      Commands::Deploy.init
     end
 
     desc 'install', 'install packages and configure the remote server with Rails, Postgres, nginx and others (run \'pops deploy\' after this)'
     def install
-      Pocketops::Commands::Deploy.install
+      Commands::Deploy.install
     end
 
     # example: pops custom post_install/task
@@ -28,17 +28,17 @@ module Pocketops
 
     desc 'config:get KEY', 'display a config value'
     define_method 'config:get' do |*keys|
-      Pocketops::Commands::Config.get(keys)
+      Commands::Config.get(keys)
     end
 
     desc 'config:set KEY1=VALUE1 [KEY2=VALUE2 ...]', 'set one or more config vars'
     define_method('config:set') do |*pairs|
-      Pocketops::Commands::Config.set(pairs)
+      Commands::Config.set(pairs)
     end
 
     desc 'config:unset KEY1 [KEY2 ...]', 'unset one or more config vars'
     define_method 'config:unset' do |*keys|
-      Pocketops::Commands::Config.unset(keys)
+      Commands::Config.unset(keys)
     end
   end
 end
